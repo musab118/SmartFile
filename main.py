5b104ff8e60cf49f8d39eeff9e9ab1af96d5a0f5
Musab Hamid <47342534+musab118@users.noreply.github.com>
2020-07-12 20:55:44 -0500

    Added safeguards

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import os
+import os.path
+import getpass
 import filetype
 import shutil
 from os import scandir
-os.listdir("/")
+username=(getpass.getuser())
 
 
 
@@ -11,19 +13,19 @@ def getpdf():
     directory = input("Enter the directory that you want to scan: ")
     directory = directory.strip()
     directory.capitalize()
-    documents = [os.listdir(F"/Users/musabhamid/{directory}")]
+    documents = [os.listdir(F"/Users/{username}/{directory}")]
     os.chdir(F"/Users/musabhamid/{directory}")
     print(os.getcwd())
     for items in documents:
         for files in items:
             if files.endswith('.pdf'):
-                if not os.path.isdir(F"/Users/musabhamid/{directory}/PDF's"):
-                    os.mkdir(F"/Users/musabhamid/{directory}/PDF's")
-                    os.chdir(F"/Users/musabhamid/{directory}")
+                if not os.path.isdir(F"/Users/{username}/{directory}/PDF's"):
+                    os.mkdir(F"/Users/{username}/{directory}/PDF's")
+                    os.chdir(F"/Users/{username}/{directory}")
                 else:
-                    os.chdir(F"/Users/musabhamid/{directory}")
+                    os.chdir(F"/Users/{username}/{directory}")
                 pdfs = files
-                shutil.move(F"/Users/musabhamid/{directory}/{pdfs}",F"/Users/musabhamid/{directory}/PDF's/{pdfs}")
+                shutil.move(F"/Users/{username}/{directory}/{pdfs}",F"/Users/{username}/{directory}/PDF's/{pdfs}")
                 print(F"The following files have been organized: {pdfs}")
 
 
@@ -31,19 +33,19 @@ def getword():
     directory = input("Enter the directory that you want to scan: ")
     directory = directory.strip()
     directory.capitalize()
-    documents = [os.listdir(F"/Users/musabhamid/{directory}")]
-    os.chdir(F"/Users/musabhamid/{directory}")
+    documents = [os.listdir(F"/Users/{username}/{directory}")]
+    os.chdir(F"/Users/{username}/{directory}")
     print(os.getcwd())
     for items in documents:
         for files in items:
             if files.endswith('.docx'):
-                if not os.path.isdir(F"/Users/musabhamid/{directory}/Word_Documents"):
-                    os.mkdir(F"/Users/musabhamid/{directory}/Word_Documents")
-                    os.chdir(F"/Users/musabhamid/{directory}")
+                if not os.path.isdir(F"/Users/{username}/{directory}/Word_Documents"):
+                    os.mkdir(F"/Users/{username}/{directory}/Word_Documents")
+                    os.chdir(F"/Users/{username}/{directory}")
                 else:
-                    os.chdir(F"/Users/musabhamid/{directory}")
+                    os.chdir(F"/Users/{username}/{directory}")
                 worddocs = files
-                shutil.move(F"/Users/musabhamid/{directory}/{worddocs}",F"/Users/musabhamid/{directory}/Word_Documents/{worddocs}")
+                shutil.move(F"/Users/{username}/{directory}/{worddocs}",F"/Users/{username}/{directory}/Word_Documents/{worddocs}")
                 print(F"The following files have been organized: {worddocs}")
 
 
@@ -51,32 +53,35 @@ def getpic():
     directory = input("Enter the directory that you want to scan: ")
     directory = directory.strip()
     directory.capitalize()
-    documents = [os.listdir(F"/Users/musabhamid/{directory}")]
-    os.chdir(F"/Users/musabhamid/{directory}")
+    documents = [os.listdir(F"/Users/{username}/{directory}")]
+    os.chdir(F"/Users/{username}/{directory}")
     print(os.getcwd())
     for items in documents:
         for files in items:
             if files.endswith('.png'):
-                if not os.path.isdir(F"/Users/musabhamid/{directory}/Photos"):
-                    os.mkdir(F"/Users/musabhamid/{directory}/Photos")
-                    os.chdir(F"/Users/musabhamid/{directory}")
+                if not os.path.isdir(F"/Users/{username}/{directory}/Photos"):
+                    os.mkdir(F"/Users/{username}/{directory}/Photos")
+                    os.chdir(F"/Users/{username}/{directory}")
                 else:
-                    os.chdir(F"/Users/musabhamid/{directory}")
+                    os.chdir(F"/Users/{username}/{directory}")
                 photos = files
-                shutil.move(F"/Users/musabhamid/{directory}/{photos}",F"/Users/musabhamid/{directory}/Photos/{photos}")
+                shutil.move(F"/Users/{username}/{directory}/{photos}",F"/Users/{username}/{directory}/Photos/{photos}")
                 print(F"The following files have been organized: {photos}")
 
 
 
 
-userinput = input('Welcome to Smart file! please type which type of files you would like to organize: ')
+userinput = input('Welcome to Smart file! please type which type of files you would like to organize: A for photos, B fpr Word Documents '
+                  'and C for PDFs  ')
 userinput = userinput.strip()
+userinput = userinput.lower()
 
 
-if userinput == 'photos':
+
+if userinput == 'a':
     getpic()
-elif userinput == 'word':
+elif userinput == 'b':
     getword()
-elif userinput == 'pdf':
+elif userinput == 'c':
     getpdf()
 
